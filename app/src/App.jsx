496f8b8ecebdcb8d1c66214ae124fdf7daae9d91
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-09-02 21:07:54 -0300

    Setup inicial React,Vite,Tailwind 4 e ShadcnUI

---
+++ app/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card flex justify-center gap-5">
+        <Button>Test shadcn4</Button>
+        <Button className='text-blue-500 '>Test tailwind4</Button>
+      </div>
+      <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
