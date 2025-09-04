77ffe17dda6061f6f00e5cbe54adde4d51eae7b2
Deewellys <deewellys@gmail.com>
2025-09-04 11:47:27 -0300

    feat dependencias
    Adicionando Axios e Sass ao projeto.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,15 +5,6 @@ import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
     <div>
-      <nav style={{ padding: "1rem", background: "#222" }}>
-        <Link to="/" style={{ marginRight: "1rem", color: "white" }}>
-          Login
-        </Link>
-        <Link to="/dashboard" style={{ color: "white" }}>
-          Dashboard
-        </Link>
-      </nav>
-
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
