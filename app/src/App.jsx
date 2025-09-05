1eb0173a86fd1c37104d2c663707647559e036eb
Deewellys <deewellys@gmail.com>
2025-09-05 11:37:40 -0300

    Feat fidelização de Tela
    Fidelizado mais ao Figma

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function App() {
   return (
