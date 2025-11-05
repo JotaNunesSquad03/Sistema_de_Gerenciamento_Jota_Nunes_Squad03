952bf6abc37a1ddcbb17efd33e9b37e787388fd8
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-11-05 13:50:11 -0300

    refactor: organize recent records table using map for cleaner and reusable structure

--- app/src/components/pages/Dashboard/partials/RecentRecords.jsx
+++ app/src/components/pages/Dashboard/partials/RecentRecords.jsx
@@ -1,9 +1,97 @@
-import {User,BookOpen} from "lucide-react";
+import {User,BookOpen,Search} from "lucide-react";
 
-export default function RecentRecords(){
+export default function RecentRecords({ handleTechnicalDetails}) {
+
+    const records =[
+        {name: "Relatório Financeiro 2025", createdBy: "Ana Silva", status: "Pendente", date: "2024-06-15"},
+        {
+            name: "Consulta SQL - Clientes Ativos",
+            createdBy: "Lucas Almeida",
+            status: "Em Revisão",
+            date: "2025-10-28",
+        },
+        {
+            name: "Formulário de Cadastro V2",
+            createdBy: "João Silva",
+            status: "Pendente",
+            date: "2025-10-25",
+        },
+        {
+            name: "Dashboard de Vendas",
+            createdBy: "Ana Souza",
+            status: "Aprovado",
+            date: "2025-10-22",
+        },
+    ]
+
+    const tableHeaders = [
+        "Nome do Registro",
+        "Criado por",
+        "Status",
+        "Data",
+        "Ações"
+    ]
+
     return(
-        <div>
-
+        <div className="registros-section">
+            <div >
+                <div >
+                    <h2>Últimos registros criados</h2>
+                    <div className="search-bar">
+                        <div className="search-input">
+                            <Search className="search-icon"/>
+                            <input type="text" placeholder="Buscar registros" className="search-field"/>
+                        </div>
+                    </div>
+                </div>
+                <table className="registros-table">
+                    <thead>
+                        <tr>
+                            {tableHeaders.map((header, index) => (
+                                <th key={index}>{header}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {records.length > 0 ? (
+                                records.map((record, index) => (
+                                    <tr key={index}>
+                                        <td>{record.name}</td>
+                                        <td>
+                                            <div className="user-info">
+                                                <div className="user-avatar sarah-avatar">
+                                                    <User/>
+                                                </div>
+                                                <span>{record.createdBy}</span>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <span className={`status-label status-${record.status.toLowerCase()}`}>
+                                                {record.status}
+                                            </span>
+                                        </td>
+                                        <td>{record.date}</td>
+                                        <td>
+                                            <div className="action-icons">
+                                                <BookOpen
+                                                    className="action-icon"
+                                                    title="Detalhes técnicos"
+                                                    onClick={()=> handleTechnicalDetails(record)}
+                                                />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))
+                            ): (
+                                <tr>
+                                    <td>
+                                        Nenhum registro encontrado.
+                                    </td>
+                                </tr>
+                            )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
